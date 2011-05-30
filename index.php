b41ed8abd7080746eda7c3b8d220f523c24db120
root <chrislee@bom.gov.au>
2011-05-30 19:05:12 +0000

    form

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+
+<form action="5840.php" method="GET">
+Region: <input type="text" name="region" value=Canberra /><br/>
+Precipitation Decile: <input type="text" name="precipDecile" value=30 /><br/>
+Precipitation Amt MM: <input type="text" name="precipAmtMm" value=444 /><br/>
+ET Decile: <input type="text" name="etDecile" value=80 /><br/>
+ET Amt MM: <input type="text" name="etAmtMm" value=777 /><br/>
+WS %: <input type="text" name="wsChangePc" value=5 /><br/>
+LWY Decile: <input type="text" name="lwDecile" value=10 /><br/>
+LWY MM: <input type="text" name="lwAmtMm" value=555 /><br/>
+SM %: <input type="text" name="smChangePc" value=10 /><br/>
+
+<input type="submit" />
+</form>
+
+</body>
+</html> 
